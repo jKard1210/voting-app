1d2245404af0ea3aba714a2fd338240581e9c9ca
James Kardatzke <kardatzkejames@yahoo.com>
2017-03-19 19:29:11 +0000

    v2

--- server.js
+++ server.js
@@ -1,87 +1,164 @@
-var express = require('express');
-var app = express()
-var multer  = require('multer');
-var storage = multer.memoryStorage()
-var bodyParser = require('body-parser');
-var upload = multer({ storage: storage })
-var passwordHash = require('password-hash');
-var fs = require('fs');
-var session = require('client-sessions');
+var express = require('express')
+  , passport = require('passport')
+  , flash = require('connect-flash')
+  , utils = require('./utils')
+  , LocalStrategy = require('passport-local').Strategy;
+var RememberMeStrategy = require('passport-remember-me').Strategy;
+
 
 
 var users = [];
 
-app.use(bodyParser());
 
-app.use(session({
-  cookieName: 'session',
-  secret: 'a359d9fj',
-  duration: 30 * 60 * 1000,
-  activeDuration: 5 * 60 * 1000,
-}));
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
 
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+/* Fake, in-memory database of remember me tokens */
+
+var tokens = {}
+
+function consumeRememberMeToken(token, fn) {
+  var uid = tokens[token];
+  // invalidate the single-use token
+  delete tokens[token];
+  return fn(null, uid);
+}
+
+function saveRememberMeToken(token, uid, fn) {
+  tokens[token] = uid;
+  return fn();
+}
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    process.nextTick(function () {
+     
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+  }
+));
+
+passport.use(new RememberMeStrategy(
+  function(token, done) {
+    consumeRememberMeToken(token, function(err, uid) {
+      if (err) { return done(err); }
+      if (!uid) { return done(null, false); }
+      
+      findById(uid, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        return done(null, user);
+      });
+    });
+  },
+  issueToken
+));
+
+function issueToken(user, done) {
+  var token = utils.randomString(64);
+  saveRememberMeToken(token, user.id, function(err) {
+    if (err) { return done(err); }
+    return done(null, token);
+  });
+}
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+var app = express()
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.engine('ejs', require('ejs-locals'));
+  app.use(express.logger());
+  app.use(express.static(__dirname + '/../../public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(flash());
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(passport.authenticate('remember-me'));
+  app.use(app.router);
+});
 
 app.get("/", function(req,res){
-     if (req.session && req.session.user) {
-         res.send("Hello Again");
-     }
-     else {
-    res.sendfile(__dirname + "/main.html");
-     }
+     res.render('index', { user: req.user });
 })
 
-app.post("/login", function(req,res){
-    res.sendFile(__dirname + "/login.html")
-})
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
 
-app.post("/signup", function(req,res){
-    res.sendFile(__dirname + "/signup.html")
-})
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user, message: req.flash('error') });
+});
 
-app.post("/newAccount", function(req,res){
-    var hashedPass = passwordHash.generate(req.body.pass);
 
-    var addition = {
-        pass: hashedPass,
-        email: req.body.email,
-        user: req.body.user
-    };
-    users.push(addition);
-    req.session.user = req.body.user;
+
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+  function(req, res, next) {
+    // Issue a remember me cookie if the option was checked
+    if (!req.body.remember_me) { return next(); }
     
-    if (req.session && req.session.user) {
-         res.send(req.session.user);
-     }
-    else {
-    res.sendFile(__dirname + "/homepage.html")
-    }
-})
+    issueToken(req.user, function(err, token) {
+      if (err) { return next(err); }
+      res.cookie('remember_me', token, { path: '/', httpOnly: true, maxAge: 604800000 });
+      return next();
+    });
+  },
+  function(req, res) {
+    res.redirect('/');
+  });
 
-app.post("/check", function(req,res){
-    var x = -1;
-    var pass = req.body.pass;
-    var user = req.body.user;
-    
-    for (var i = 0 ; i < users.length; i++) {
-        if (user == users[i].user) {
-            x = i;
-        }
-    }
-    
-    if (x == -1) {
-        res.send("Error: Incorrect Username");
-    }
-    else {
-        if (passwordHash.verify(pass, users[x].pass)) {
-            req.session.user = user;
-            res.sendFile(__dirname + "/homepage.html")
+app.get('/logout', function(req, res){
+  // clear the remember me cookie when logging out
+  res.clearCookie('remember_me');
+  req.logout();
+  res.redirect('/');
+});
 
-        }
-        else {
-            res.send("Error: Incorrect Password");
-        }
-    }
-})
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
