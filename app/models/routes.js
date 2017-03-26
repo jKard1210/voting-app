3c199aecd6f901f783339663e0c3557a7eac372c
James Kardatzke <kardatzkejames@yahoo.com>
2017-03-26 01:30:43 +0000

    v2

--- app/models/routes.js
+++ app/models/routes.js
@@ -13,7 +13,7 @@ module.exports = function(app, passport) {
 
 
 app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/home', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
@@ -24,12 +24,45 @@ app.post('/login', passport.authenticate('local-login', {
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
     
+    
    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/account', // redirect to the secure profile section
+        successRedirect : '/home', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
+    app.get('/home', isLoggedIn, function(req, res) {
+        var mongoose = require('mongoose');
+        var db = require('../../config/database.js');
+ 
+        
+        var Schema = mongoose.Schema;
+         var poll = new Schema({
+                title: String,
+         });
+
+        var PollModel = mongoose.model('polls', poll);
+        var poll = mongoose.model("polls");
+
+
+        poll.find({}, function(err, data){
+        if (err) console.log(err);
+        console.log(">>>> " + data);
+        res.render('home.ejs', {
+            user: req.user,
+            data: data
+        });
+    });
+
+    });
+    
+    app.get('/create', isLoggedIn, function(req, res) {
+        res.render('create.ejs', {
+            user: req.user
+        });
+    });
+    
+    
     app.get('/account', isLoggedIn, function(req, res) {
         res.render('account.ejs', {
             user : req.user // get the user out of session and pass to template
